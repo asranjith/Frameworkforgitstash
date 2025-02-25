fdfa34f893c1b4b28c95827358591c81e35a1b5c
asranjith <rockyranjith27@gmail.com>
2025-02-25 12:02:49 +0530

    customer is added

---
+++ src/test/java/com/comcast/crm/customer/CustomerTest.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.customer;
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	@Test
+	public void customerTest() {
+		System.out.println("customer is god");
+	}
+
+}
