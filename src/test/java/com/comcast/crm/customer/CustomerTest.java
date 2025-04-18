e77848749fbcc30c6a0690d2fbf94a177c80b4ac
asranjith <rockyranjith27@gmail.com>
2025-04-18 11:25:05 +0530

    customer push

--- src/test/java/com/comcast/crm/customer/CustomerTest.java
+++ src/test/java/com/comcast/crm/customer/CustomerTest.java
@@ -5,7 +5,23 @@ import org.testng.annotations.Test;
 public class CustomerTest {
 	@Test
 	public void customerTest() {
-		System.out.println("customer is god");
+	     String s="asdf12345!@#$%^";
+	     String a="";
+	     String n="";
+	     String q="";
+	     for(int i=0;i<s.length();i++){
+	         char ch=s.charAt(i);
+	         if(ch>='a' && ch<='z' || ch>='A' && ch<='Z'){
+	             a+=s.charAt(i);
+	         }else if(ch>='1' && ch<='9'){
+	             n+=s.charAt(i);
+	         }else{
+	             s+=s.charAt(i);
+	         }
+	     }
+	     System.out.println("alphabet"+a);
+	     System.out.println("number"+n);
+	     System.out.println("special Character"+q);
 	}
 
 }
